8bcac506b3d706378f7c36b9940fa5696933bd08
Habiba <habibaelhadi9@gmail.com>
2025-02-01 18:22:11 +0200

    implementing signup method

--- Plateful/app/src/main/java/com/example/plateful/MainActivity.java
+++ Plateful/app/src/main/java/com/example/plateful/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.plateful;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.signupbtn);
+        FirebaseUser current = auth.getCurrentUser();
+
+        button.setOnClickListener(view ->{
+            signup();
         });
     }
+
+    public void signup(){
+        auth.createUserWithEmailAndPassword("habibaelhadi9@gmail.com","12345")
+                .addOnCompleteListener(task -> {
+                    Toast.makeText(this,"success",Toast.LENGTH_LONG).show();
+                }).addOnFailureListener(task ->{
+                    Toast.makeText(this,"fail",Toast.LENGTH_LONG).show();
+
+                });
+    }
+
+
 }
