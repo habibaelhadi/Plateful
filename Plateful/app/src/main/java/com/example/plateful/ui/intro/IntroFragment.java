a9f67dfbafa297d2c92687b214bf29e2d866ee30
Habiba <habibaelhadi9@gmail.com>
2025-02-05 11:56:27 +0200

    Designing intro page

---
+++ Plateful/app/src/main/java/com/example/plateful/ui/intro/IntroFragment.java
@@ -0,0 +1,33 @@
+package com.example.plateful.ui.intro;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.plateful.R;
+
+
+public class IntroFragment extends Fragment {
+
+
+    public IntroFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro, container, false);
+    }
+}
